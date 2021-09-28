45ddc3cf3d826f00ec57a8a773d383b5bbe01cf4
erik-greenblomroos-nti-johanneberg <74712714+erik-greenblomroos-nti-johanneberg@users.noreply.github.com>
2021-09-28 10:52:16 +0200

    gradient på banner

--- src/Home/style.js
+++ src/Home/style.js
@@ -2,5 +2,6 @@ import styled from "styled-components";
 export const Banner = styled.section`
     width: 100%;
     height: 500px;
-    background-color: purple;
+    background: linear-gradient(300deg, #B09DF4, 60%, #6521D3);
+
 `
