7279c6dc79633a36b7078bd2270a8e44e5afeb2f
erik-greenblomroos-nti-johanneberg <74712714+erik-greenblomroos-nti-johanneberg@users.noreply.github.com>
2021-10-04 17:27:24 +0200

    Home

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import LoginButton from "./LoginButton";
 
-const Banner = () => {
+const Banner = ({ children }) => {
     return (
         <section className="p-4 bg-purple" style={{
             width: '100%',
             height: '500px'
         }}>
+            <div className="flex justify-center">
+                <div className="w-auto flex-col flex items-center">
+                    <h1 className="text-4xl text-white">The faster way to learn</h1>
+                    <h2 className="text-2xl text-white">Easily use scientifically proven learning techniques</h2>
+                    <LoginButton />
+                </div>
+            </div>
 
         </section>
     );
 }
- 
+
 export default Banner;
