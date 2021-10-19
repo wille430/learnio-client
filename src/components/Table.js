cbc3a605d1804a0198f5e72663b280640b73d929
william-wigemo-nti-johanneberg <william.wigemo@elev.ga.ntig.se>
2021-10-19 09:50:00 +0200

    fix: center expand button

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import TableButton from "components/TableButton"
 
-
-const Table = ({ headers }) => {
+const Table = ({ headers, rows }) => {
     return (
         <div classname="w-full ">
             <table className="mx-auto border-solid border-2 border-gray-400 bg-gray-100 table-fixed" style={{
                 width: '90%',
             }}>
                 <tr className="border-solid border-2 border-gray-400 text-sm m-1">
-                    <th className="w-1/16">Curcle</th>
+                    {/* <th className="w-1/16">Curcle</th>
                     <th className="w-3/16">Course Name</th>
-                    <th className="w-7/16 flex justify-start">Description</th>
+                    <th className="w-7/16 text-left">Description</th>
                     <th className="w-2/16">Active Techniques</th>
                     <th className="w-2/16">Deadline</th>
-                    <th className="w-1/16">...</th>
+                    <th className="w-1/16">...</th> */}
+                    {
+                        headers.map(header => {
+                            return <th className="text-xs mx-1 text-left">{header}</th>
+                        })
+                    }
                 </tr>
                 <tr>
                     {
                         headers.map(header => {
-                            return <th className="text-xs m-1"> {header}
-                            </th>
+                            return <th className="text-xs mx-1">{header}</th>
                         })
                     }
                 </tr>
-                <tr className="flex justify-center col-span-6">
-                    <TableButton />
+                <tr>
+                    <td colSpan="5" className="text-center py-2">
+                        <TableButton />
+                    </td>
                 </tr>
             </table>
         </div>
--- src/components/TableButton.js
+++ src/components/TableButton.js
@@ -1,11 +1,9 @@
 import React from 'react';
-const TableButton = ({children}) => {
+const TableButton = () => {
     return (
-        
-            <h3 className="text-md text-gray-600 bg-gray-300 rounded-md w-12 flex justify-evenly h-4 items-center"> 
-            <div>• • •</div>
-            </h3>
-
+        <button className="text-lg mx-auto text-gray-600 bg-gray-300 rounded-md w-12 flex justify-evenly h-4 items-center">
+            <span>• • •</span>
+        </button>
     );
 }
 
