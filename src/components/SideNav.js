9cbbd80bdcfb73e995d0aa582baacad3a2ccf749
erik-greenblomroos-nti-johanneberg <74712714+erik-greenblomroos-nti-johanneberg@users.noreply.github.com>
2021-12-07 11:08:44 +0100

    mailbutton

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -8,7 +8,7 @@ const SideNav = () => {
         <aside className="side-nav h-screen sticky bg-purple shadow-sm p-5 text-white space-y-1 pt-8 top-0" style={{
             width: '215px'
         }}>
-            <h3>Navbar</h3>
+            <h3>Dashboard</h3>
             <ul className="flex flex-col space-y-2 pl-4">
                 <Link to="/"><li>Courses +</li></Link>
                 <Link to="/create-project"><li>Create course</li></Link>
